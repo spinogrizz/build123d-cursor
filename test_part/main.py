ce76210ea40b99ae94d7afac167d011136528e5c
Denis Gryzlov <gryzlov@gmail.com>
2025-11-04 02:21:45 +0300

    more fixes to run on save

--- test_part/main.py
+++ test_part/main.py
@@ -1,9 +1,11 @@
-length, width, thickness = 80.0, 40.0, 8.0
+length, width, thickness = 80.0, 30.0, 1.0 
 
 sk3 = Circle(width) - Rectangle(length / 2, width / 2)
 ex3 = extrude(sk3, amount=2 * thickness)
 
+test = Circle(10) + Rectangle(10, 10) + Circle(15)
+ex4 = extrude(test, amount=2 * thickness)
+
+ex3 += ex4
 
 result = ex3
-
-
