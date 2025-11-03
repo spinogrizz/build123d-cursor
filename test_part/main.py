f64bbb5c59c5c8b2fa9af93a1d337bda32e70d28
Denis Gryzlov <gryzlov@gmail.com>
2025-11-04 02:47:47 +0300

    comments

--- test_part/main.py
+++ test_part/main.py
@@ -1,4 +1,4 @@
-length, width, thickness = 80.0, 30.0, 1.0 
+length, width, thickness = 80.0, 30.0, 10.0 
 
 sk3 = Circle(width) - Rectangle(length / 2, width / 2)
 ex3 = extrude(sk3, amount=2 * thickness)
